acbeb494327319e6368e643367ec9ff3b12d61ec
sehugg <sehugg@05812258-7f48-0410-9316-dfcb6471b382>
2008-03-24 01:59:03 +0000

    added shipping.ini for missions

--- Exoflight/scripts/run.bat
+++ Exoflight/scripts/run.bat
@@ -4,4 +4,6 @@ set HEIGHT=768
 set FULLSCRN=true
 set MAINCLASS=com.fasterlight.exo.main.Exoflight
 set CPATH=lib/FLCore.jar;lib/Exoflight.jar;lib/jogl.jar;lib/gluegen-rt.jar;lib/joal.jar;lib/jinput.jar;lib/png.jar;.
-java -Djava.library.path=.\lib -Dexo.fullscreen=%FULLSCRN% -Dexo.scrnwidth=%WIDTH% -Dexo.scrnheight=%HEIGHT% -cp %CPATH% %MAINCLASS%
+set VMARGS="-Djava.library.path=.\lib -Dexo.fullscreen=%FULLSCRN% -Dexo.scrnwidth=%WIDTH% -Dexo.scrnheight=%HEIGHT%"
+set ARGS="-stdout stdout.txt -stderr stderr.txt"
+java %VMARGS% -cp %CPATH% %MAINCLASS% %ARGS%
